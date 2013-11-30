16bbba8c6a0cbdfcb76b0276e9b5e5035a6491d3
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2013-11-30 12:35:41 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ocalApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
